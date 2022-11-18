cb0719660d44c08c01b75e9226144286f88985ae
RamyAlmutaeri <118525334+RamyAlmutaeri@users.noreply.github.com>
2022-11-18 14:49:31 +0300

    Add files via upload

---
+++ Very Large Scale Integration project/Phase2/Design implmention/case10/ALU/32inALU.cmd
@@ -0,0 +1,12 @@
+stepsize 50
+ana Vdd Gnd enabel a15 a14 a13 a12 a11 a10 a9 a8 a7 a6 a5 a4 a3 a2 a1 a0 b15 b14 b13 b12 b11 b10 b9 b8 b7 b6 b5 b4 b3 b2 b1 b0 Cin Cout d15 d14 d13 d12 d11 d10 d9 d8 d7 d6 d5 d4 d3 d2 d1 d0 
+w Vdd Gnd enabel a15 a14 a13 a12 a11 a10 a9 a8 a7 a6 a5 a4 a3 a2 a1 a0 b15 b14 b13 b12 b11 b10 b9 b8 b7 b6 b5 b4 b3 b2 b1 b0 Cin Cout d15 d14 d13 d12 d11 d10 d9 d8 d7 d6 d5 d4 d3 d2 d1 d0 
+h Vdd
+l Gnd
+h enabel
+h Cin
+vector A a15 a14 a13 a12 a11 a10 a9 a8 a7 a6 a5 a4 a3 a2 a1 a0
+vector B b15 b14 b13 b12 b11 b10 b9 b8 b7 b6 b5 b4 b3 b2 b1 b0
+setvector A 1110000000001011
+setvector B 1101000000000111
+s
