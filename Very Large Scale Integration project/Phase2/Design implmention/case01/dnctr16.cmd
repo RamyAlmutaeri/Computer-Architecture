6304b833624bb749633d7cf0c27deace6280c5b6
RamyAlmutaeri <118525334+RamyAlmutaeri@users.noreply.github.com>
2022-11-18 14:20:22 +0300

    Add files via upload

---
+++ Very Large Scale Integration project/Phase2/Design implmention/case01/dnctr16.cmd
@@ -0,0 +1,60 @@
+stepsize 150
+ana vdd gnd reset phi1 phi2 nxta nxtb nxtc nxtd nextEn En
+w   vdd gnd reset phi1 phi2 nxta nxtb nxtc nxtd nextEn En
+clock phi1	0 0 0 1 0
+clock phi2	1 0 0 0 0
+h vdd
+l gnd 
+h reset 
+l En
+c
+l reset 
+h En
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
+c
+s
+
---
+++ Very Large Scale Integration project/Phase2/Design implmention/case11/case11.cmd
@@ -0,0 +1,15 @@
+stepsize 50
+ana Vdd Gnd  in0 in1 in2 in3 in4 in5 in6 in7 out0 out1 out2 out3 out4 out5 out6 out7 out8 out9 out10 out11 out12 out13 out14 out15
+w  Vdd Gnd  in0 in1 in2 in3 in4 in5 in6 in7 out0 out1 out2 out3 out4 out5 out6 out7 out8 out9 out10 out11 out12 out13 out14 out15
+h Vdd
+l Gnd
+s
+l in0 
+l in1 
+l in2 
+l in3
+l in4
+h in5
+h in6
+h in7
+s
