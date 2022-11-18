32c3a87e8647e622b79d004f063ff01013094a23
RamyAlmutaeri <118525334+RamyAlmutaeri@users.noreply.github.com>
2022-11-18 14:28:21 +0300

    Add files via upload

---
+++ Very Large Scale Integration project/Phase2/Design implmention/case10/Multiplier/8in-Multiplier/8in-multiplier.cmd
@@ -0,0 +1,19 @@
+stepsize 50
+ana Vdd Gnd s reset phi1 phi2 in0 in1 in2 in3 in4 in5 in6 in7 out0 out1 out2 out3 out4 out5 out6 out7
+w  Vdd Gnd s reset phi1 phi2 in0 in1 in2 in3 in4 in5 in6 in7 out0 out1 out2 out3 out4 out5 out6 out7
+clock phi1	1 0 0 0
+clock phi2	0 0 1 0
+h Vdd
+l Gnd
+l reset
+h s
+c
+l in0 
+l in1 
+l in2 
+l in3 
+l in4 
+l in5 
+l in6 
+h in7 
+c
